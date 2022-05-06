26577d088511d3ef6255ea799f20f4398547d731
Nathalia Schneider <81602411+nathischneider03@users.noreply.github.com>
2022-05-05 20:27:05 -0400

    Add files via upload

---
+++ NSRDW2/java.js
@@ -0,0 +1,9 @@
+console.log("MOIN");
+var array=[1,2,3,4,5,6];
+var suma=0;
+for(var i=0; i<array.length; i++)
+{
+suma=suma+array[i];
+}
+console.log("Los numeros son: "+array);
+console.log("La suma es: "+s);
---
+++ NSRDW2/tarea2403.js
@@ -0,0 +1,18 @@
+console.log("HELLO"); /*una sola tabla*/
+console.log("_________________________________________");
+var tabla=[5];
+console.log("La tabla a multiplicar es: "+tabla);
+for(var i=1; i<11; i++){
+console.log("5x"+i+"="+tabla*i);
+}
+
+console.log("_________________________________________");//tablas desde un numero a otro
+var tabla1=[2];
+var tabla2=[5]
+if(tabla1<tabla2){
+console.log("El primer valor no puede ser mayor que el segundo");
+}
+
+
+
+
