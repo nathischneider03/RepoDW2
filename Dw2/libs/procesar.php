f376124c24ca9ccebfdde1b88ea079d519b23eaa
Nathalia Schneider <81602411+nathischneider03@users.noreply.github.com>
2022-05-06 11:50:00 -0400

    Update procesar.php

--- Dw2/libs/procesar.php
+++ Dw2/libs/procesar.php
@@ -1,50 +1,41 @@
-<pre>
 <?php
-//if ($_POST['nombre']=="usuario" and $_POST["contranha"]=="segundo")
-//{
-//print_r($_POST);
-//} else
-//{
-//echo "usted no tiene permitido ver el contenido";
-
-//}
-$errores=[];
-$error=0;
-if($_POST)
+$ errores =[];
+$ error = 0 ;
+//FILTER_SANITIZE_FULL_SPECIAL_CHARS
+si ( $ _POST )
 {
-
-if ($_POST['nombre']=="")
+  si ( $ _POST [ 'nombre' ]== "" )
+  {
+    $ error ++;
+    array_push ( $ errores , "El nombre no debe estar vacio" );
+  }
+  if ( $ _POST [ 'apellido' ]== "" )
+  {
+    $ error ++;
+    array_push ( $ errores , "El apellido no debe estar vacio" );
+  }
+  $ nfecha = date_parse ( $ _POST [ 'fenac' ]);
+  if ( $ nfecha [ 'error_count' ])
+   {
+     $ error ++;
+     array_push ( $ errores , "la fecha debe ser valida" );
+   }
+   if (! filter_var ( $ _POST [ 'ecorreo' ], FILTER_VALIDATE_EMAIL )){
+       $ error ++;
+       array_push ( $ errores , "la el email debe ser valido" );
+   }
+si ( $ error > 0 ) {
+  print_r ( $ errores );
+} más
 {
-$error++;
-array_push($errores,"El nombre no debe estar vacio");
+  echo  "la solicitud fue procesada" ;
 }
 
-if ($_POST['apellido']=="")
-{
-$error++;
-array_push($errores,"El apellido no debe estar vacio");
+} más {
+echo  "la solicitud no puede ser procesada" ;
 }
 
-$nfecha=date_parse($_POST['fenac']);
-if ($nfecha['error_count'])
-{
-$error++;
-array_push($errores,"La fecha debe ser valida");
-}
 
-if (filter_var($_POST['ecorreo'],FILTER_VALIDATE_EMAIL))
-{
-$error++;
-array_push($errores,"El email debe ser valido");
-}
 
-if ($error>0){
-	print_r($errores);
-}
-
-} else {
-echo "la solicitud no puede ser aceptada";
-}
 
  ?>
- </pre>
